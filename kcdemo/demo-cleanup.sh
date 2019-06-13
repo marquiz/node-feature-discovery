4a4507f11b79dc7aa2423270ad33a6daa5fce22b
Markus Lehtonen <markus.lehtonen@intel.com>
2019-06-13 13:54:55 +0300

    demo: add cleanup script

---
+++ kcdemo/demo-cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+ns="default"
+if [ -n "$1" ]; then
+    ns=$1
+fi
+
+# Remove objects
+kubectl -n $ns delete ds/nfd-master \
+               ds/nfd-worker \
+               svc/nfd-master \
+               clusterrolebinding/nfd-master \
+               clusterrole/nfd-master \
+               sa/nfd-master
+
+# Remove annotations and labels
+kubectl get no -o yaml | sed -e '/^\s*nfd.node.kubernetes.io/d' -e '/^\s*feature.node.kubernetes.io/d' | kubectl replace -f -
