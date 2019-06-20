1cf8c0b66569e822cb446dc42e004d0e68603b98
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2019-06-20 15:53:23 +0300

    demo: watch intel gpu resources

--- kcdemo/watch-nodes.sh
+++ kcdemo/watch-nodes.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-watch --color "kubectl get no -o json | jq -C -r '.items[] | {name:.metadata.name, labels:.metadata.labels, annotations:.metadata.annotations}' | grep -v -e '\"beta.kubernetes.io' -e '.alpha.kubernetes.io' -e 'volumes.kubernetes.io' -e '\"kubernetes.io'"
+kubectl_cmd="kubectl get no -o json"
+jq_cmd="jq -C -r '.items[] | {name:.metadata.name, labels:.metadata.labels, annotations:.metadata.annotations, allocatable: {\"gpu.intel.com/i915\": .status.allocatable.\"gpu.intel.com/i915\"}}'"
+grep_cmd="grep -v -e '\"beta.kubernetes.io' -e '.alpha.kubernetes.io' -e 'volumes.kubernetes.io' -e '\"kubernetes.io'"
+watch --color "$kubectl_cmd | $jq_cmd | $grep_cmd"
